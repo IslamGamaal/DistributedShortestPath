ff0e3142bbdde0c17fbee9ec913ad29c845e327a
karim-mohamed <k.mohamed3@hotmail.com>
2020-04-04 00:03:52 +0200

    Adding graph size function for client

--- src/Graph.java
+++ src/Graph.java
@@ -3,4 +3,5 @@ import java.rmi.*;
 interface Graph extends Remote {
     String initGraph(String edges) throws RemoteException;
     String apply (String[] batchLines) throws RemoteException;
+    int getGraphSize();
 }
